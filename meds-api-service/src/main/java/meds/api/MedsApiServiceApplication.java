5da09327569fea19e05e1727e8282e7ec4ae56c9
Aliaksei Sery <aliaksei.sery@gmail.com>
2024-04-25 20:35:37 -0400

    First commit with basic GET/POST operations

---
+++ meds-api-service/src/main/java/meds/api/MedsApiServiceApplication.java
@@ -0,0 +1,13 @@
+package meds.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedsApiServiceApplication.class, args);
+	}
+
+}
---
+++ meds-api-service/src/test/java/meds/api/MedsApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package meds.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedsApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
